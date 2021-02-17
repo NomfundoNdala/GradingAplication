6769de276309b5349d138e3d27888f2baf3f9e58
academy_learner <nomfundo@thedigitalacademy.co.za>
2021-02-17 09:58:21 +0200

    Added components

--- ClientSide/src/app/app.module.ts
+++ ClientSide/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard/dashboard.component';
+import { RegisterComponent } from './components/register/register.component';
+import { RubricComponent } from './components/rubric/rubric.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    RegisterComponent,
+    RubricComponent
   ],
   imports: [
     BrowserModule,
---
+++ ClientSide/src/app/components/rubric/rubric.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rubric',
+  templateUrl: './rubric.component.html',
+  styleUrls: ['./rubric.component.scss']
+})
+export class RubricComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

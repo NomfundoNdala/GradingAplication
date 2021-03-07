04e2756301eb4cf58cbb6b0ac6326e56f4f4223d
prince matuane <55387933+princematumane@users.noreply.github.com>
2021-03-07 21:14:22 +0200

    new chnages in app modules allows exports

--- gradApplication/src/app/app.module.ts
+++ gradApplication/src/app/app.module.ts
@@ -10,6 +10,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './Components/home/home.component';
 import { MatTableModule } from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+const modules = [
+  MatButtonModule,
+  MatFormFieldModule,
+  BrowserModule,
+  AppRoutingModule,
+  ReactiveFormsModule,
+  HttpClientModule,
+  MatTableModule,
+  BrowserAnimationsModule
+];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,14 +31,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     RegisterComponent,
     HomeComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    MatTableModule,
-    BrowserAnimationsModule
-  ],
+  imports: [...modules],
+  exports: [...modules],
   providers: [],
   bootstrap: [AppComponent]
 })

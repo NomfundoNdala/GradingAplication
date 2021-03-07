ec4bc3013d668c551f471df3f9f0508793037ebd
prince matuane <55387933+princematumane@users.noreply.github.com>
2021-03-07 21:24:04 +0200

    routing

--- gradApplication/src/app/app-routing.module.ts
+++ gradApplication/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './Components/login/login.component';
 import { RegisterComponent } from './Components/register/register.component';
 
 const routes: Routes = [
-
+  { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent }];

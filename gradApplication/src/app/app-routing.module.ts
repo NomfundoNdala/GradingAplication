a2d39c28bd24c2df7ad543538547abcbb15db701
prince matuane <55387933+princematumane@users.noreply.github.com>
2021-03-18 08:52:39 +0200

    app rounting and new component for edit student

--- gradApplication/src/app/app-routing.module.ts
+++ gradApplication/src/app/app-routing.module.ts
@@ -7,8 +7,8 @@ import { RegisterComponent } from './Components/register/register.component';
 import { StudentComponent } from './Components/student/student.component';
 import { LecturerComponent } from './Components/lecturer/lecturer.component';
 import { GroupComponent } from './Components/group/group.component';
-import { EditStudentComponent } from './Components/edit-student/edit-student.component';
 import { ReportsComponent } from './Components/reports/reports.component';
+import { StudentEditComponent } from './Components/student-edit/student-edit.component';
 
 
 
@@ -22,8 +22,8 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'lecturer', component: LecturerComponent },
   { path: 'group', component: GroupComponent },
-  // { path: 'reports', component: ReportsComponent },
-  // { path: 'editS/:id', component: EditStudentComponent },
+  { path: 'reports', component: ReportsComponent },
+  { path: 'editS/:id', component: StudentEditComponent },
   { path: 'assignment', component: AssigmentManagerComponent },
   { path: 'group/:id', component: AssigmentManagerComponent }];
 

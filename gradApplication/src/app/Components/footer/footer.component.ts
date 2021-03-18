aecc6107aea1e90465dbf1c126d103be072b226e
Nomfundo <nomfundozama88@gmail.com>
2021-03-18 02:13:16 +0200

    socila icons

--- gradApplication/src/app/Components/footer/footer.component.ts
+++ gradApplication/src/app/Components/footer/footer.component.ts
@@ -6,13 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-  private stackoverflowUrl: string = 'https://stackoverflow.com/users/1791913/faisal?tab=profile';
+ 
   private linkedinUrl: string = 'https://www.linkedin.com/in/mohammadfaysal/';
-  private fiverrUrl: string = 'https://www.fiverr.com/faisalmuhammad_';
-  private facebookUrl: string = 'https://www.facebook.com/phaysall';
-  private twitterUrl: string = 'https://twitter.com/faisssallll';
-  private githubUrl: string = 'https://github.com/faisalmuhammad';
-  private googleUrl: string = 'https://plus.google.com/+MuhammadFaisal04';
+  private twitterUrl: string = 'https://twitter.com/Grader86183323/';
+ 
   constructor() { }
 
   ngOnInit(): void {
